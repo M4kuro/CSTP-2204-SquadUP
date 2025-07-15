ddb9713c69a05fe1ffe363c6732ece7ffebaaafb
Devl1977 <000478938@student.vcc.ca>
2025-07-14 17:59:10 -0700

    Fix Stripe webhook route + add publicDir for Vite static routingin vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // adding this line below for a failsafe for static webs 
+  // (redirect after stripe wasn't working for some reason)
+   publicDir: 'public',
   
   //adding  this section to route /api calls to backend
   server: {
